688a41b72b346e9c75d9470ffffe9055e1e82695
evezmemmedli <avazkm@code.edu.az>
2022-02-09 23:07:22 +0400

    Add project files.

---
+++ 09.02.2022 task rep/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._02._2022_task_rep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //if (number < 0)
+            //{
+            //    Console.WriteLine("Musbet Eded Daxil");
+            //}
+
+            //List<int> numbers = new List<int>();
+
+            //switch (number)
+            //{
+            //    case < 50:
+            //        {
+            //            for (int i = 1; i <= number; i++)
+            //            {
+            //                if (i % 3 == 0)
+            //                {
+            //                    numbers.Add(i);
+
+            //                }
+            //            }
+            //            break;
+            //        }
+            //    case < 100:
+            //        {
+            //            for (int i = 1; i <= number; i++)
+            //            {
+            //                if (i % 5 == 0)
+            //                {
+            //                    numbers.Add(i);
+            //                }
+            //            }
+            //            break;
+            //        }
+            //    default:
+            //        {
+
+            //            for (int i = 1; i <= number; i++)
+            //            {
+            //                if (i % 8 == 0)
+            //                {
+            //                    numbers.Add(i);
+            //                }
+            //            }
+            //            break;
+            //        }
+            //}
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            ////-------------------------------------------------------------------------------------
+
+            //string[] words = {"salam","baku","chair","window","armchair"};
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    if (words[i].Length > 5)
+            //    {
+            //        Console.WriteLine(words[i]);
+            //    }
+            //}
+            ////--------------------------------------------------------------------------------------
+            //int number = 246;
+            //int remain = 0;
+            //while (number!=0)
+            //{
+            //    remain += number % 10;
+            //    number=number/10;   
+            //}
+            //Console.WriteLine(remain);
+        }
+    }
+}
